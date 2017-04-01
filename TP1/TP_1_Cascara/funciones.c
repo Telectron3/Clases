d57211a145d4277550d24438df9dd670eb41a015
Telectron3 <tincho_martin1996@hotmail.com>
2017-04-01 13:04:16 -0300

    Add files via upload

---
+++ TP1/TP_1_Cascara/funciones.c
@@ -0,0 +1,29 @@
+
+float pedirNumero()
+{
+    float numero;
+    printf("Ingrese un  numero: ");
+    scanf("%f",&numero);
+    return numero;
+}
+
+ float suma(float numero1,float numero2)
+{
+    return numero1 + numero2;
+}
+
+float resta(float numero1,float numero2)
+{
+    return numero1 - numero2;
+}
+
+float divicion(float numero1, float numero2)
+{
+    return numero1/numero2;
+}
+
+float multiplicacion(float numero1, float numero2)
+{
+    return numero1*numero2;
+}
+
---
+++ TP1/TP_1_Cascara/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+float pedirNumero();
+float suma(float,float);
+float resta(float,float);
+float multiplicacion(float,float);
+float divicion(float,float);
+int factorial();
+
+#endif // FUNCIONES_H_INCLUDED
